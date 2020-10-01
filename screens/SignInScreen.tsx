c72c54067583e8f45ef42869ffde38da0bd2555a
marieneubrander <mdmcleroy@crimson.ua.edu>
2020-10-01 16:06:39 -0500

    Add welcome message

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,13 +1,15 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { RootStackParamList } from '../types';
 
 const SignInScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sign in.</Text>
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Welcome to TES Verify!</Text>
+      </View>
     </View>
   );
 }
@@ -15,22 +17,69 @@ export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    flex: 1, 
+    backgroundColor: '#009387'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  header: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      paddingHorizontal: 20,
+      paddingBottom: 50
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
+  footer: {
+      flex: 3,
+      backgroundColor: '#fff',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      paddingHorizontal: 20,
+      paddingVertical: 30
   },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  text_header: {
+      color: '#fff',
+      fontWeight: 'bold',
+      fontSize: 30
   },
+  text_footer: {
+      color: '#05375a',
+      fontSize: 18
+  },
+  action: {
+      flexDirection: 'row',
+      marginTop: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#f2f2f2',
+      paddingBottom: 5
+  },
+  actionError: {
+      flexDirection: 'row',
+      marginTop: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: '#FF0000',
+      paddingBottom: 5
+  },
+  textInput: {
+      flex: 1,
+      marginTop: Platform.OS === 'ios' ? 0 : -12,
+      paddingLeft: 10,
+      color: '#05375a',
+  },
+  errorMsg: {
+      color: '#FF0000',
+      fontSize: 14,
+  },
+  button: {
+      alignItems: 'center',
+      marginTop: 50
+  },
+  signIn: {
+      width: '100%',
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10
+  },
+  textSign: {
+      fontSize: 18,
+      fontWeight: 'bold'
+  }
 });
