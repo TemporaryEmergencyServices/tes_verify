7570e8a89074857958dd925dc4a6e6c74618d54a
marieneubrander <mdmcleroy@crimson.ua.edu>
2020-10-01 15:41:28 -0500

    Add SignInScreen

---
+++ screens/SignInScreen.tsx
@@ -0,0 +1,36 @@
+import { StackScreenProps } from '@react-navigation/stack';
+import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { RootStackParamList } from '../types';
+
+const SignInScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign in.</Text>
+    </View>
+  );
+}
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
