076ecd549d9db5280eb26439029f1c01ad9ce1a1
marieneubrander <mdmcleroy@crimson.ua.edu>
2020-10-01 15:28:26 -0500

    Remove device location functionality

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,67 +8,11 @@ import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions';
 import MapView, { AnimatedRegion } from 'react-native-maps'
 import { Marker } from 'react-native-maps'
+import TabTwoScreen from './TabTwoScreen';
 
 
 export default function TabOneScreen() {
-  const [location, setLocation] = useState({})
-  const [errorMsg, setErrorMsg] = useState('')
-
-  const getUserLocation = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    
-    let region
-    if (status !== 'granted') {
-      // default coords
-      region = {
-        latitude: 33.95644567711626,
-        longitude: -84.78240539428366,
-        latitudeDelta: 1,
-        longitudeDelta: 1,
-      }
-    } else {
-      const location = await Location.getCurrentPositionAsync({});
-      region = {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 1,
-        longitudeDelta: 1,
-      }
-    }
-    return {status, region}
-  }
-
-  useEffect(() => {
-    (async () => {
-      const {status, region} = await getUserLocation()
-      if (status !== 'granted') {
-        setErrorMsg('Location permission denied')
-      }
-      console.log('region', region)
-      setLocation(region)
-    })()
-  }, [])
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      <Text>{JSON.stringify(location)}</Text>
-      { location != null && 
-        <MapView style={styles.mapStyle}
-          region={location}
-        >
-        <Marker 
-          coordinate={location}
-          title={"My Location"}
-          description={"desc"}
-        />
-      </MapView>
-      }
-      
-    </View>
-  );
+  return TabTwoScreen();
 }
 
 const styles = StyleSheet.create({
