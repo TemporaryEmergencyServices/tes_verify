6179a311c11b571ab21300736fa9f3365b1d23fb
Marie Neubrander <mneubrander@crimson.ua.edu>
2020-10-12 12:12:51 -0500

    Added useEffect() ability to pull all clock ins and outs on records screen

--- screens/RecordsScreen.tsx
+++ screens/RecordsScreen.tsx
@@ -1,15 +1,39 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions, Button, TouchableOpacity, Alert, FlatList } from 'react-native';
+import { useEffect, useState, Component } from 'react'
 
-import EditScreenInfo from '../components/EditScreenInfo';
+import firebase from '../firebase.js'
+
 import { Text, View } from '../components/Themed';
+// import { analytics } from 'firebase';
+import { useSelector, useDispatch, RootStateOrAny } from 'react-redux'
 
 export default function RecordsScreen() {
+
+  const user = useSelector((state: RootStateOrAny) => state.user)
+  const userEmail = user.email
+
+  const [loading, setLoading] = useState(true);
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    var ref = firebase.database().ref("ClockInsOuts/")
+    var test = []
+    ref.on("value", function(snapshot){
+      console.log(snapshot.val());
+      //setRecords(snapshot.val())
+     // test = snapshot.val()
+
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
+    //console.log(test)
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Export a log of your volunteer hours here!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-
+      <Text style={styles.title}>All clock ins and outs are currently being logged to console. Am unsure on how to get them on the screen.</Text>
     </View>
   );
 }
