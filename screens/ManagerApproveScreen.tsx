0b8ccf00c4d0ecc15df75ce8af5d893ee84408bb
Brandon Lucas <thebrandonlucas@gmail.com>
2020-10-26 19:51:03 -0500

    get pending manager approval

--- screens/ManagerApproveScreen.tsx
+++ screens/ManagerApproveScreen.tsx
@@ -2,30 +2,56 @@ import * as React from 'react';
 import { StyleSheet, Dimensions, Button, TouchableOpacity, Alert } from 'react-native';
 import { useEffect, useState } from 'react'
 
+import firebase from '../firebase.js'
 import '@firebase/firestore';
 
 import { Text, View } from '../components/Themed';
 import { useSelector, useDispatch, RootStateOrAny } from 'react-redux'
 
 export default function ClockInScreen() {
-  const [uniqueClockID, setUniqueClockID] = useState('')
-  const user = useSelector((state: RootStateOrAny) => state.user)
-  const userEmail = user.email
+
+  const [loading, setLoading] = useState(true)
+  const [records, setRecords] = useState([] as any)
 
   useEffect(() => {
-
+    const subscriber = firebase.firestore()
+    .collection('ClockInsOuts')
+    .where('in_approved' , '==', 'pending')
+    .where('out_approved', '==', 'pending')
+    .onSnapshot(querySnapshot => {
+      const helperRecords = [] as any;
+      querySnapshot.forEach(documentSnapshot => {
+        helperRecords.push({
+        ...documentSnapshot.data(),
+        key: documentSnapshot.id
+        });
+      });
+      setRecords(helperRecords);
+      setLoading(false);
+  });
+  return () => subscriber();
   }, [])
 
   /*
     TODO: 
-    - create manager approved navigator
+    - capture all pending
+    - allow for option to approve/deny
+    - allow for editing (separate screen?)
   */
-
-  return (
-    <View style={styles.container}>
-      <Text>Manager Approvals</Text>
-    </View>
-  )
+  if (loading) {
+    return (
+      <View>
+        <Text>Sorry, I'm still loading...</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Manager Approvals</Text>
+        <Text>{JSON.stringify(records)}</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
