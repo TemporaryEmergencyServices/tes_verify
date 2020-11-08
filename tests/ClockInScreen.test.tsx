50a468905cbf0e51708314c83dde361239339466
Brandon Lucas <thebrandonlucas@gmail.com>
2020-11-07 22:14:26 -0600

    default user test

--- tests/ClockInScreen.test.tsx
+++ tests/ClockInScreen.test.tsx
@@ -7,8 +7,18 @@ import { store } from '../App'
 jest.useFakeTimers()
 
 describe("<ClockInScreen />", () => {
+    const wrapper = renderer.create(<Provider store={store} ><ClockInScreen /></Provider>).toJSON()
+
     it('renders correctly', () => {
-        const tree = renderer.create(<Provider store={store} ><ClockInScreen /></Provider>).toJSON();
-        expect(tree).toMatchSnapshot();
+        // const tree = renderer.create(<Provider store={store} ><ClockInScreen /></Provider>).toJSON()
+        expect(wrapper).toMatchSnapshot()
     });
+
+    it('defaults to user not having access', () => {
+        console.log(wrapper.children[0].children[0])
+        const noAccessText = wrapper.children[0].children[0]
+        expect(noAccessText === ' You are not authorized :( ').toBe(true)
+    })
+
+    // it('clicks clock in' () =)
 });
