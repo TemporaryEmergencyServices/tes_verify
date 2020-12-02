f17b528047a74c64bf256ce7530dc7b5a3919c9c
Marie Neubrander <mneubrander@crimson.ua.edu>
2020-12-01 22:53:40 -0600

    added search button to clock in search page

--- screens/QueryClockScreen.tsx
+++ screens/QueryClockScreen.tsx
@@ -1,35 +1,169 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
 
 import { RootStackParamList } from '../types';
 
 export default function QueryClockScreen({ navigation }) {
+
+  const handleSearch = () => {
+    Alert.alert(
+        'HELLO',
+        'HELLO:)',
+         [
+           {text: 'OK', onPress: () => {console.log('OK Pressed'); }},
+         ],
+         {cancelable: false},
+       );
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Search for records by name, userid, gender, ethnicity, and/or date.</Text>
+      <Text style={styles.instructions}>Search for records by name, userid, gender, ethnicity, and/or date.</Text>
+
+      <TouchableOpacity style={styles.loginBtn} onPress={handleSearch}>
+        <Text style={styles.signUpText} >SEARCH</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
+    scrollview: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      justifyContent: 'center',
+      textAlign: 'center',
+      paddingTop: 60,
+      paddingBottom: 15,
+      color: "#1C5A7D",
+      
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#FFFFFF',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    contentContainer: {
+      backgroundColor: '#FFFFFF',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 300
+    },
+  
+    logo:{
+      fontWeight:"bold",
+      fontSize:24,
+      color:"#1C5A7D",
+      textAlign: 'center',
+      marginTop: 60,
+      paddingRight: 30, 
+      paddingLeft: 30
+    },
+  
+    instructions:{
+      fontSize:18,
+      color:"#1C5A7D",
+      marginBottom:40,
+      textAlign: 'center',
+      paddingRight: 7,
+      paddingLeft: 7
+    },
+    inputView:{
+      width:"90%",
+      backgroundColor:"#2B2E32",
+      borderRadius:25,
+      height:50,
+      marginBottom:20,
+      justifyContent:"center",
+      padding: 20,
+      paddingRight: 40,
+      
+    },
+    inputText:{
+      height:50,
+      color:"white"
+    },
+    forgot:{
+      color:"white",
+      fontSize:11
+    },
+    loginBtn:{
+      width:"80%",
+      backgroundColor:"#E11383",
+      borderRadius:25,
+      height:50,
+      alignItems:"center",
+      justifyContent:"center",
+  
+      marginBottom:10
+    },
+    createAccountBtn:{
+      width:"80%",
+      backgroundColor:"#E11383",
+      borderRadius:25,
+      height:50,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop:10,
+      marginBottom:10
+    },
+    returnToSignInText:{
+      color:"black"
+    },
+    signUpText:{
+      color:"white",
+      fontSize: 18,
+      fontWeight: "bold", 
+    },
+    input: {
+      margin: 5,
+      padding: 6,
+      borderRadius: 8,
+      marginBottom: 8,
+      paddingHorizontal: 10,
+      backgroundColor: "#eceff1",
+    }
+  })
+  
+  const modalstyles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5
+    },
+    openButton: {
+      backgroundColor: "#F194FF",
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    textStyle: {
+      color: "black",
+      fontWeight: "bold",
+      textAlign: "left",
+      fontSize: 18
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
